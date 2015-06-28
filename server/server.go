93596e1faa2d84b0965057dde192d3c2ffe3660f
CodingAnarchy <mtanous22@gmail.com>
2015-06-28 02:39:52 -0600

    Modify Go version of server to return IP address of associated domain name

--- server/server.go
+++ server/server.go
@@ -5,24 +5,39 @@ import(
   "fmt"
   "net"
   "bufio"
+  "strings"
 )
+
+var domains map[string]net.IP
 
 func handleConnection(conn net.Conn) (bool) {
   reader := bufio.NewReader(conn)
   writer := bufio.NewWriter(conn)
+  var ip string
   fmt.Println("Reading from connection...")
   for {
     msg, err := reader.ReadString('\n')
     if err != nil {
       log.Fatal("Error receieving from client: ", err)
     }
-    writer.WriteString(msg)
+    msg = strings.TrimSuffix(msg, "\n")
+    if addr, ok := domains[msg]; ok {
+      ip = addr.String()
+    } else {
+      ip = "Domain not found."
+    }
+    writer.WriteString(fmt.Sprint(ip, "\n"))
     writer.Flush()
   }
 }
 
 func main() {
   fmt.Println("Server starting...")
+  fmt.Println("Creating map of domains to IP addresses...")
+  domains = make(map[string]net.IP)
+  domains["www.google.com"] = net.ParseIP("74.125.224.72")
+  domains["www.facebook.com"] = net.ParseIP("69.63.176.13")
+  domains["example.com"] = net.ParseIP("93.184.216.119")
   listener, err := net.Listen("tcp", ":8080")
   if err != nil {
     log.Fatal(err)
